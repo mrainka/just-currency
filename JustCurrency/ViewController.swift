20f8f36bea6deb2f6593cdd1398e6ed4aad42ea2
Marcin Rainka <marcin.rainka@gmail.com>
2020-04-18 08:09:44 +0200

    Add empty project

---
+++ JustCurrency/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JustCurrency
+//
+//  Created by Marcin Rainka on 18/04/2020.
+//  Copyright © 2020 Marcin Rainka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
